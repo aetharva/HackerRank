a0c954cbaaff52f255b7e0a547d4723deda98cd6
aetharva <65614958+aetharva@users.noreply.github.com>
2022-07-05 23:35:10 +0530

    Delete Hackerrank Student Advisor.sql

--- Hackerrank Student Advisor.sql
+++
@@ -1,5 +0,0 @@
-SELECT si.roll_number, si.name
-FROM student_information si
-JOIN faculty_information fi
-ON si.advisor = fi.employee_id
-WHERE (fi.gender = 'M' AND fi.salary > 15000) OR (fi.gender = 'F' AND fi.salary > 20000)
