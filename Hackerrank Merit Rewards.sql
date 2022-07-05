a8748cc36081bc74560ab73bed9f1e70cf63b2d8
aetharva <65614958+aetharva@users.noreply.github.com>
2022-07-05 23:35:03 +0530

    Delete Hackerrank Merit Rewards.sql

--- Hackerrank Merit Rewards.sql
+++
@@ -1,5 +0,0 @@
-SELECT employee_information.employee_ID, employee_information.name
-FROM employee_information
-JOIN last_quarter_bonus
-ON employee_information.employee_ID = last_quarter_bonus.employee_ID
-WHERE employee_information.division = 'HR' AND last_quarter_bonus.bonus >= 5000
